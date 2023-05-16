e463dc57c4fafe70a128872ea9c5066a88ca7196
wooyong <jwooy00@naver.com>
2023-05-16 10:11:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/notification1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.notification1;
+
+import static java.util.logging.Level.parse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String NOTIFICATION_CHANNEL_ID = "my channel_id_01";
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Channel description");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createNotificationChannel();
+    }
+    public void sendNotification(View view){
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("메일 알림")
+                .setContentText("새로운 메일이 도착하였습니다.")
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notificationBuilder.build());
+    }
+}
